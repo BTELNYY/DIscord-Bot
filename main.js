d1c489cb2e67f76cd4418a461c3e84557ac76759
BTELNYY <74313030+BTELNYY@users.noreply.github.com>
2022-05-16 00:22:28 -0600

    Added -slowmode command

--- main.js
+++ main.js
@@ -25,33 +25,41 @@ client.on("messageCreate", msg => {
         return;
     }else{
         var cmddecompiled = msg.toString().split(' ')
-        var command = msg.toString().slice(1)
+        var command = cmddecompiled[0].toString().slice(1)
         switch(command){
+            case "slowmode":
+                if(!msg.member.permissions.has("MANAGE_CHANNELS")){
+                    msg.reply("You don't have permission to do that.")
+                    return;
+                }
+                if(cmddecompiled.length < 2){
+                    msg.reply("Specify number of seconds after the command.")
+                    return
+                }
+                var limit = parseInt(cmddecompiled[1])
+                msg.channel.setRateLimitPerUser(limit)
+                msg.reply(`Updated channel slowmode to ${limit}`)
+                break
             case "lockdown":
-                if((typeof msg === Discord.Message)){
-                    throw new Error("Invalid Parameter for function commandLockdown!")
+                var channel = msg.channel.id
+                if(!msg.member.permissions.has("MANAGE_CHANNELS")){
+                    command.reply("You don't have permission to do that.")
+                    return;
+                }
+                if(!channels.hasOwnProperty(channel)){
+                    channels[channel] = true
+                    status = true
                 }else{
-                    var command = Object.assign(msg, Discord.Message)
-                    var channel = msg.channel.id
-                    if(!msg.member.permissions.has("ADMINISTRATOR")){
-                        command.reply("You don't have permission to do that.")
-                        return;
-                    }
-                    if(!channels.hasOwnProperty(channel)){
-                        channels[channel] = true
-                        status = true
-                    }else{
-                        status = !channels[channel]
-                    }
-                    if(status){
-                        var embed = Utility.basicEmbed("Channel Lockdown", "#FF0000", "This channel is locked down.", "btelnyy-bot")
-                        msg.channel.send({ embeds: [embed]})
-                        msg.channel.permissionOverwrites.create(msg.channel.guild.roles.everyone, { SEND_MESSAGES: false , EMBED_LINKS: false, ATTACH_FILES: false, SEND_MESSAGES_IN_THREADS: false, USE_APPLICATION_COMMANDS: false, CREATE_PUBLIC_THREADS: false});
-                    }else{
-                        var embed = Utility.basicEmbed("Channel Lockdown Lifted", "00FF00", "This channel can be used as normal.", "btelnyy-bot")
-                        msg.channel.send({ embeds: [embed]})
-                        msg.channel.permissionOverwrites.edit(msg.channel.guild.roles.everyone, { SEND_MESSAGES: null , EMBED_LINKS: null, ATTACH_FILES: null, SEND_MESSAGES_IN_THREADS: null, USE_APPLICATION_COMMANDS: null, CREATE_PUBLIC_THREADS: null});
-                    }
+                    status = !channels[channel]
+                }
+                if(status){
+                    var embed = Utility.basicEmbed("Channel Lockdown", "#FF0000", "This channel is locked down.", "btelnyy-bot")
+                    msg.channel.send({ embeds: [embed]})
+                    msg.channel.permissionOverwrites.create(msg.channel.guild.roles.everyone, { SEND_MESSAGES: false , EMBED_LINKS: false, ATTACH_FILES: false, SEND_MESSAGES_IN_THREADS: false, USE_APPLICATION_COMMANDS: false, CREATE_PUBLIC_THREADS: false});
+                }else{
+                    var embed = Utility.basicEmbed("Channel Lockdown Lifted", "00FF00", "This channel can be used as normal.", "btelnyy-bot")
+                    msg.channel.send({ embeds: [embed]})
+                    msg.channel.permissionOverwrites.edit(msg.channel.guild.roles.everyone, { SEND_MESSAGES: null , EMBED_LINKS: null, ATTACH_FILES: null, SEND_MESSAGES_IN_THREADS: null, USE_APPLICATION_COMMANDS: null, CREATE_PUBLIC_THREADS: null});
                 }
                 break
             case "ping":
